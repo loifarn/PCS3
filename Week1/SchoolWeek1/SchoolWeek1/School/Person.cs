13b13b42603ded62f656ebd54da13ffd78878188
Alex_Pedersen <i390141@fhict.nl>
2017-09-19 12:21:55 +0200

    Week 1 - 1.2 A

---
+++ Week1/SchoolWeek1/SchoolWeek1/School/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+    class Person
+    {
+
+        private string name;
+        private string person;
+        private string pcn;
+        private int age;
+        private int yearsAtFontys;
+
+        public Person(string name, string pcn, int age)
+        {
+            this.name = name;
+            this.pcn = pcn;
+            this.age = age;
+        }
+
+
+        public void StartAnotherSchoolYear()
+        {
+            yearsAtFontys++;
+        }
+
+        public void CelebrateBirthday()
+        {
+            age++;
+        }
+    }
+}
