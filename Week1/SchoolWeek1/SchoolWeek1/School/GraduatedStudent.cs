328bf81e91b3ae40286fe957a78278e2593de838
Alex_Pedersen <i390141@fhict.nl>
2017-09-19 14:41:28 +0200

    Week 1 - 1.3 B

---
+++ Week1/SchoolWeek1/SchoolWeek1/School/GraduatedStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+    class GraduatedStudent : Person
+    {
+        private int YearOfGraduation;
+        private double GraduationMark;
+
+        public GraduatedStudent(int yearOfGraduation, double graduationMark, string name, string address, int pcn, int age) : base(name, address, pcn, age)
+        {
+            this.Name = name;
+            this.PCN = pcn;
+            this.Age = age;
+            this.Address = address;
+            this.YearOfGraduation = yearOfGraduation;
+            this.GraduationMark = graduationMark;
+        }
+    }
+}
